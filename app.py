e699693eed72d616dd7c219090c3bf82d97fce53
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-23 12:24:30 +0200

    z

--- app.py
+++ app.py
@@ -4,16 +4,30 @@ st.title('DriverCheck')
 
 
 def main():
-    check = st.text_input(
-        "Please type 'yes' if driver license is present, 'no' if missing: ")
+    st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background-image: url("https://trafficlawfirm.com/wp-content/uploads/2019/05/How-Do-I-Check-if-My-License-Is-Suspended-in-Florida.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
 
-    if check == "yes":
-        st.text('User  allowed to drive')
 
-    if check == "no":
-        st.text('User not allowed to drive')
-    if check != "no" and check != 'yes' and check != "":
-        st.text('Command not recognized')
+check = st.text_input(
+    "Please type 'yes' if driver license is present, 'no' if missing: ")
+
+if check == "yes":
+    st.text('User  allowed to drive')
+
+if check == "no":
+    st.text('User not allowed to drive')
+if check != "no" and check != 'yes' and check != "":
+    st.text('Command not recognized')
 
 
 if __name__ == '__main__':
