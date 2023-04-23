c9e028ea08657be258cf0070ac0a459f7441246f
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-23 10:44:37 +0200

    streamlit exercise

---
+++ app copy.py
@@ -0,0 +1,6 @@
+def main():
+    print('Type 1 if river license is present, 0 if missing: ')
+
+
+if __name__ == '__main__':
+    main()
---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title('DriverCheck')
+
+
+def main():
+    check = input('Please type 1 if river license is present, 0 if missing: ')
+
+    if check == "1":
+        print('User  allowed to drive')
+
+        if check == "0":
+            print('User not allowed to drive')
+    else:
+        print('Command not recognized')
+
+
+if __name__ == '__main__':
+    main()
