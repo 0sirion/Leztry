3bda0f15595bc1d415508e4a776c9edad6b75ec9
0sirion <salvatore.arcuri.9@gmail.com>
2023-04-23 11:34:33 +0200

    exercise

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ st.title('DriverCheck')
 
 
 def main():
-    check = st.text('hello!')
+    check = st.text_input(
+        'Please type 1 if river license is present, 0 if missing: ')
+
+    if check == "1":
+        st.text('User  allowed to drive')
+
+        if check == "0":
+            st.text('User not allowed to drive')
+    else:
+        st.text('Command not recognized')
 
 
 if __name__ == '__main__':
